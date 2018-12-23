baf8c9cf36be0601dd02c04c9af02c5189627daf
takekid <kid@kidnoMacBook-puro.local>
2018-12-23 12:08:18 +0900

    Devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 
 def edit
 end
@@ -12,6 +12,7 @@ def update
 end
 
 private
+
 def user_params
   params.require(:user).permit(:name, :email)
 end
