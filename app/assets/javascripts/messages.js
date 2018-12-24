6263f1a6ee92ce613659f28c4420c73b0f6c2d16
takekid <kid@kidnoMacBook-puro.local>
2018-12-24 13:19:44 +0900

    Message send

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,6 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="message">
+                       <div class="upper-message">
+                           <div class="upper-message__user-name">
+                               ${ message.user_name }
+                           </div>
+                           <div class="upper-message__date">
+                               ${ message.time }
+                           </div>
+                       </div>
+                       <div class="lower-message">
+                           <p class="lower-message__content">
+                               ${ message.content }
+                           </p>
+                       </div>
+                   </div>`;
+    return html;
+  }
+  function scroll() {
+    $('.messages').animate({scrollTop: $('.message')[0].scrollHeight});
+}
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      .var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
+    })
   });
-});
