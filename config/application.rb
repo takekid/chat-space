48f1029553a71cdee9a92dcbfa854dde8eb36737
takekid <kid@kidnoMacBook-puro.local>
2019-01-06 16:50:56 +0900

    Comit

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,8 @@
 require_relative 'boot'
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
